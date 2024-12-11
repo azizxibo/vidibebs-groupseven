eb4d81bf984f2396b6f669d9924242e7671b73e1
Aziz Ramdhani <aziz.ramdhani@student.president.ac.id>
2024-12-11 23:53:48 +0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 testing vidibebs)
+st.title("🎈 testing vidibebs")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
